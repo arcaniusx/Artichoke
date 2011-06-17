78b14c93c670748cbe31013b527b271f8ed8c6ed
Filipe Araujo <filipe.alves.araujo@gmail.com>
2011-06-17 12:48:28 -0400

    initial commit

---
+++ js/artichoke/artichoke-1.0.js
@@ -0,0 +1,27 @@
+/**
+ * Artichoke visual testing suite
+ * @description
+ * @required jQuery
+ * @author Filipe Araujo
+ * @version 1.0
+ */
+
+
+
+
+
+
+Artichoke = (function($){
+    var
+    api = {
+        test : function(){
+
+        }
+    };
+
+    return api();
+}(jQuery));
+
+
+
+
