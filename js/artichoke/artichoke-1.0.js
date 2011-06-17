40cb18e369ded63a63680bac98538df8aa42833a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2011-06-17 15:05:22 -0400

    added isVisible and exists API

--- js/artichoke/artichoke-1.0.js
+++ js/artichoke/artichoke-1.0.js
@@ -13,13 +13,52 @@
 
 Artichoke = (function($){
     var
-    api = {
-        test : function(){
 
-        }
+    result = function(msg){
+        return (!msg) ? test = { msg : 'Test passed', result : true } : { msg : msg, result : false };
+    },
+        
+    api = function(el){
+        var
+        
+        $el = $(el),
+
+        exists = function(){
+            var msg,
+                selectors = el.split(','),
+                xl = selectors.length -1;
+
+            for(xl; xl>-1; xl--){
+                if($(selectors[xl]).length === 0){
+                    msg = 'Expected selector:'+selectors[xl]+' to exist';
+                }
+            }
+
+            return result(msg);
+
+        },
+
+        visible = function(){
+            var msg;
+            $el.each(function(){
+                if($(this).is(':hidden')){
+                    msg = 'Expected selector:'+$el.selector+' to be visible';
+                }
+            });
+            
+            return result(msg);
+        };
+
+
+
+        return {
+            exists : exists,
+
+            isVisible : visible
+        };
     };
 
-    return api();
+    return api;
 }(jQuery));
 
 
